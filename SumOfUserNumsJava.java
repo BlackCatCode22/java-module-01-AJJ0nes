7ae3429efa4aa6426c9beed41ab379e3cd8ead32
AJJ0nes <0869525@my.scccd.edu>
2025-08-21 10:13:57 -0700

    Add files via upload

---
+++ SumOfUserNumsJava.java
@@ -0,0 +1,54 @@
+
+//SumOfUserNums.java
+//AJJ 8/19/25
+// resources https://www.w3schools.com/java/java_user_input.asp
+
+
+// Get inputs from user and output the sum of inputs
+
+import java.util.Scanner;
+
+public class SumOfUserNumsJava {
+    public static void main(String[] args) {
+        System.out.println("\n\n Welcome to my Largest of 3 Values");
+
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+
+
+        Scanner scanner = new Scanner(System.in);     //create the scanner
+        System.out.println("\n Enter Num1 value: ");  // prompt the user
+        num1 = scanner.nextInt();                     // collects int from user and overwrites inital variable
+        System.out.println("\n num 1 is: " + num1);   // returns the value to the user
+
+        System.out.println("\n Enter Num2 value: ");
+        num2 = scanner.nextInt();
+        System.out.println("\n num 2 is: " + num2);
+
+        System.out.println("\n Enter Num3 value: ");
+        num3 = scanner.nextInt();
+        System.out.println("\n num 3 is: " + num3);
+
+//largest of all three
+        if(num1 > num2) {
+            if(num1 > num3) {
+                System.out.println("\n The Largest Value is Num1 at: " + num1);
+            }else{
+                System.out.println("\n The Largest Value is Num3 at: " + num3);
+            }
+        }else if(num2 > num3) {
+            System.out.println("\n The Largest Value is Num2 at: " + num2);
+        }else{
+            System.out.println("\n The Largest Value is Num 3 at: " + num3);
+        }
+
+
+
+
+
+
+
+
+    }
+}
